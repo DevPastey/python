8f49b8dcdd3d3743f0a5d6f28aa88ba38e889f55
DevPastey <emmanuelolabodee@outlook.com>
2025-12-30 21:24:17 +0100

    first commit

---
+++ email_simulator.py
@@ -0,0 +1,15 @@
+class Email:
+    def __init__(self, sender, receiver, subject, body):
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.body = body
+        self.read = False
+
+    def mark_as_read(self):
+        self.read = True
+
+email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!')
+print(email_obj.sender)
+print(email_obj.subject)
+print(email_obj.read)
