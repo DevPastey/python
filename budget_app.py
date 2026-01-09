5bb9bf37dc0477989d0f45f8d47822b2586faeb9
DevPastey <emmanuelolabodee@outlook.com>
2026-01-09 12:26:51 +0100

    fix Category methods

--- budget_app.py
+++ budget_app.py
@@ -9,19 +9,22 @@ class Category:
         )
 
     def withdraw(self, amount, description=""):
-        if amount > 0:
+        if self.check_funds(amount):
             self.ledger.append({'amount': -amount, 'description': description})
             return True
         return False
 
     def get_balance(self):
-        return self.ledger
+        total = 0
+        for item in self.ledger:
+            total += item['amount']
+        return total
 
     def transfer(self, amount, destination_category):
         withdrawal_description = f"Transfer to {destination_category.name}"
-        
-        if self.withdraw(amount, withdrawal_description):
 
+        if self.check_funds(amount):
+            self.withdraw(amount, withdrawal_description)
             deposit_description = f"Transfer {self.name}"
 
             destination_category.deposit(amount, deposit_description)
@@ -30,28 +33,21 @@ class Category:
     
 
     def check_funds(self, amount):
-        if amount > self.leger:
+        if amount > self.get_balance():
             return False
         return True
 
     def __str__(self):
 
-        title_line = f"{'*' * ((30 - len(self.name)) // 2)}{self.name}{'*' * ((30 - len(self.name)) // 2)} \ninitial"
-        # Ledger entries, left-aligned description and right-aligned amount (2 decimal places)
-        ledger_lines = ""
-        for transaction in self.ledger:
-            description = transaction['description'][:23]  # Description max 23 characters
-            amount = f"{transaction['amount']:>7.2f}"  # Right-align amount, 2 decimal places
-            ledger_lines += f"{description:<23}{amount}\n"
-
-        # Final balance line
-        total_balance = f"Total: {self.get_balance():.2f}"
-
-        # Combine everything
-        return f"{title_line}\n{ledger_lines}{total_balance}"
-
-
-
+        title = f"{self.name:*^30}\n"
+        items = ""
+        for entry in self.ledger:
+            amount = f"{entry['amount']:>7.2f}"
+            description = entry['description'][:23]
+            items += f"{description:<23}{amount:>7}\n"
+        total = f"Total: {self.get_balance():.2f}"
+        return title + items + total
+    
 def create_spend_chart(categories):
     pass
 
@@ -61,8 +57,6 @@ food.withdraw(10.15, 'groceries')
 food.withdraw(15.89, 'restaurant and more food for dessert')
 clothing = Category('Clothing')
 food.transfer(50, clothing)
-
-# Print categories after transactions
 print(food)
 
     # print( food.deposit(900, 'deposit') and food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread') )
